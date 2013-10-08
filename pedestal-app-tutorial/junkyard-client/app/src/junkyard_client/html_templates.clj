ee8c87a25576bf2c850b22996eee684a147e4bd2
Sammons <bsammnz@gmail.com>
2013-10-07 21:45:14 -0500

    fixed problem with ns in repl

--- pedestal-app-tutorial/junkyard-client/app/src/junkyard_client/html_templates.clj
+++ pedestal-app-tutorial/junkyard-client/app/src/junkyard_client/html_templates.clj
@@ -1,10 +1,7 @@
-(ns ^:shared junkyard-client.html-templates
-  (:require [io.pedestal.app.templates :only [tfn dtfn tnodes]]))
+(ns junkyard-client.html-templates
+  (:use [io.pedestal.app.templates :only [tfn dtfn tnodes]]))
 
 (defmacro junkyard-client-templates
   []
-  {:junkyard-client-page (dtfn (tnodes "junkyard-client.html" "hello") #{:id})
-   :other-counter (dtfn (tnodes "tutorial-client.html" "other-counter") #{:id}
-  })
-
- (defn whan [x] (+ x 2))
+  {:junkyard-client-page (dtfn (tnodes "junkyard-client.html" "tutorial" [[:#other-counters]]) #{:id})
+   :other-counter (dtfn (tnodes "junkyard-client.html" "other-counter") #{:id})})
